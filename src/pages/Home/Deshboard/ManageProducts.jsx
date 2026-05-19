8e27cce3c94d970ba90fe1c235031d0b64e3fe73
Labib1851 <labibabeed2007@gmail.com>
2026-05-19 23:35:47 +0600

    fix issu

--- src/pages/Home/Deshboard/ManageProducts.jsx
+++ src/pages/Home/Deshboard/ManageProducts.jsx
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 
-const AddProduct = () => {
-    const [formData, setFormData] = useState({
-        name:"",
-        price: "",
-        image: "",
-        category: "",
-        description: ""
-    })
+ 
+ const ManageProducts = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    price: "",
+    image: "",
+    category: "",
+    description: "",
+  });
 
-
-     const handleChange = (e) => {
+  const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -35,13 +35,11 @@ const AddProduct = () => {
     });
   };
 
-    const ManageProducts = () => {
-    return (
-     
+  return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Manage Product</h1>
+      <h1 className="text-2xl font-bold mb-4">Add Product</h1>
 
-        <form onSubmit={handleSubmit} className="space-y-3">
+      <form onSubmit={handleSubmit} className="space-y-3">
 
         <input name="name" value={formData.name} onChange={handleChange} placeholder="Name" className="border p-2 w-full" />
 
@@ -62,38 +60,7 @@ const AddProduct = () => {
 
       </form>
     </div>
-    );
+  );
 };
-}
-
-// const ManageProducts = () => {
-//     return (
-     
-//     <div>
-//       <h1 className="text-2xl font-bold mb-4">Manage Product</h1>
-
-//         <form onSubmit={handleSubmit} className="space-y-3">
-
-//         <input name="name" value={formData.name} onChange={handleChange} placeholder="Name" className="border p-2 w-full" />
-
-//         <input name="price" value={formData.price} onChange={handleChange} placeholder="Price" className="border p-2 w-full" />
-//         <input name="price" value={formData.price} onChange={handleChange} placeholder="Price" className="border p-2 w-full" />
-
-//         <input name="image" value={formData.image} onChange={handleChange} placeholder="Image URL" className="border p-2 w-full" />
-//         <input name="size" value={formData.image} onChange={handleChange} placeholder="Image URL" className="border p-2 w-full" />
-
-//         <input name="category" value={formData.category} onChange={handleChange} placeholder="Category" className="border p-2 w-full" />
-
-//         <textarea name="description" value={formData.description} onChange={handleChange} placeholder="Description" className="border p-2 w-full" />
-
-//         <button type="submit" className="bg-black text-white px-4 py-2">
-//           Add Product
-//         </button>
-        
-
-//       </form>
-//     </div>
-//     );
-// };
 
 export default ManageProducts;
